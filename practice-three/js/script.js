0ef3f98966ad79d24b57d7853a098345218e1ce4
Truls Johan Jacobsen-Lamøy <>
2024-02-13 15:20:35 +0100

    made some changes

--- practice-three/js/script.js
+++ practice-three/js/script.js
@@ -2,19 +2,18 @@ const container = document.querySelector("#catsContainer");
 console.log(container);
 
 let html = "";
-let dataResult = "";
+let dataResult = [];
 fetch(`https://catfact.ninja/facts`)
     .then(response => response.json())
     .then(result => {
         dataResult = result;
         console.log("dataResult: ", dataResult);
         for (let i = 0; i < dataResult.length; i++){
-            html += `<div class="catsDisplay">
+            container.innerHTML += `<div class="catsDisplay">
                         <h1 class="catsTitle">title: ${dataResult[i].title}</h1>
                         <h2 class="catsLength">length: ${dataResult[i].length}</h2>
                      </div>
             `
-            container.innerHTML = html;
         }
     });
 
